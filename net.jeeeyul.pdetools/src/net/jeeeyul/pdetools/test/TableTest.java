87d8f1ecc6854a0a181d213be9073230287b4aad
Jeeeyul <jeeeyul@gmail.com>
2012-08-21 17:41:41 +0900

    Signed-off-by: Jeeeyul <jeeeyul@gmail.com>

--- net.jeeeyul.pdetools/src/net/jeeeyul/pdetools/test/TableTest.java
+++ net.jeeeyul.pdetools/src/net/jeeeyul/pdetools/test/TableTest.java
@@ -1,35 +1,32 @@
 package net.jeeeyul.pdetools.test;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
-import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Event;
-import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Shell;
-import org.eclipse.swt.widgets.Table;
-import org.eclipse.swt.widgets.TableItem;
 
 public class TableTest {
 	public static void main(String[] args) {
 		Display display = Display.getDefault();
-		Shell shell = new Shell();
-		shell.setLayout(new GridLayout());
+		final Shell shell = new Shell();
+		shell.setLayout(new FillLayout());
 
-		final Table table = new Table(shell, SWT.SINGLE | SWT.FULL_SELECTION);
-		table.addListener(SWT.MeasureItem, new Listener() {
-			@Override
-			public void handleEvent(Event event) {
-				event.width = table.getClientArea().width;
-			}
-		});
-		table.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-		table.setLinesVisible(true);
+		StyledText text = new StyledText(shell, SWT.MULTI);
+		text.setLayoutData(new GridData(GridData.FILL_BOTH));
 
-		for (int i = 0; i < 10; i++) {
-			TableItem tableItem = new TableItem(table, SWT.NONE);
-			tableItem.setText("fuck");
-		}
+		Image image = new Image(display, 20, text.getLineHeight());
+		GC gc = new GC(image);
+		gc.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
+		gc.setAlpha(10);
+		gc.setLineStyle(SWT.LINE_DASH);
+		gc.setLineDash(new int[] { 2, 2 });
+		gc.drawLine(0, 0, 20, 0);
+		gc.dispose();
+		text.setBackgroundImage(image);
 
 		shell.open();
 
