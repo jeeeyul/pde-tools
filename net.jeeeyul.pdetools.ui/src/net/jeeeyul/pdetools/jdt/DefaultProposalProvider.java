0b32959c7cd249c3e4890ab26b962d4c06bba717
Jeeeyul <jeeeyul@gmail.com>
2014-02-19 00:06:28 +0900

    sematic content assist experiments

--- net.jeeeyul.pdetools.ui/src/net/jeeeyul/pdetools/jdt/DefaultProposalProvider.java
+++ net.jeeeyul.pdetools.ui/src/net/jeeeyul/pdetools/jdt/DefaultProposalProvider.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.ui.text.java.ContentAssistInvocationContext;
 import org.eclipse.jdt.ui.text.java.IJavaCompletionProposalComputer;
+import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
+import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 import org.eclipse.jface.text.contentassist.IContextInformation;
 
@@ -19,7 +21,18 @@ public class DefaultProposalProvider implements IJavaCompletionProposalComputer 
 	@Override
 	public List<ICompletionProposal> computeCompletionProposals(ContentAssistInvocationContext context,
 			IProgressMonitor monitor) {
+		JavaContentAssistInvocationContext jCtx = (JavaContentAssistInvocationContext) context;
 		ArrayList<ICompletionProposal> result = new ArrayList<ICompletionProposal>();
+
+		if (jCtx.getExpectedType() != null) {
+			if (jCtx.getExpectedType().getFullyQualifiedName().equals("org.eclipse.swt.graphics.Color")) {
+				try {
+					result.add(new ColorProposal(jCtx, jCtx.computeIdentifierPrefix().length()));
+				} catch (BadLocationException e) {
+					e.printStackTrace();
+				}
+			}
+		}
 
 		return result;
 	}
@@ -27,7 +40,8 @@ public class DefaultProposalProvider implements IJavaCompletionProposalComputer 
 	@Override
 	public List<IContextInformation> computeContextInformation(ContentAssistInvocationContext context,
 			IProgressMonitor monitor) {
-		return null;
+		ArrayList<IContextInformation> result = new ArrayList<IContextInformation>();
+		return result;
 	}
 
 	@Override
